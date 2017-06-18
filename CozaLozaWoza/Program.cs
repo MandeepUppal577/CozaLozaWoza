f0d6c639a4e1d318dc9d58daf00cf2c9a9185abc
MandeepUppal577 <mandeepuppal131@gmail.com>
2017-06-18 07:34:45 -0400

    numbers divisible by 5 and 7

--- CozaLozaWoza/Program.cs
+++ CozaLozaWoza/Program.cs
@@ -10,6 +10,24 @@ namespace CozaLozaWoza
     {
         static void Main(string[] args)
         {
+            int start = 1, int end = 110;
+
+            for (int i = start; i <= end; ++i)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Coza");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza");
+                }
+
+            }
         }
     }
 }
